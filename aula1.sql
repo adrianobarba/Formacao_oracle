2e0875d4ed0be1a0470e89a89082b687c08a8c3d
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-03-08 07:47:02 -0300

    commit inicial

---
+++ aula1.sql
@@ -0,0 +1,15 @@
+create, que cria uma base de dados ou tabela.
+alter, que altera as propriedades da estrutura de um dos componentes do banco. Esse componente pode ser um índice, uma tabela ou até um banco.
+truncate apaga de forma definitiva os dados de uma tabela.
+drop apaga o componente da estrutura de dados.
+
+
+insert, que inclui dados dentro de uma tabela.
+update, que altera dados de uma tabela já existente.
+delete, que apaga os dados de uma tabela.
+lock, que gerencia a concorrência de atualização de dados na mesma tabela
+
+
+commit que salva o estado do banco de dados de forma definitiva no disco.
+rollback retorna o estado de um banco de dados relacional a partir de um ponto salvo previamente.
+savepoint que salva esse ponto prévio no banco de dados.
